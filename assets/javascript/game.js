198e9c900d7f14474a2a33c510dc155b07d369f5
Carolyn Vasisko <carolyn@mothermedia.com>
2018-02-18 17:18:39 -0600

    psuedo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+//Crystal Collectors Game Javascript//
+
+//aka CrystalMath...It's Add-ictive---//
+
+//Game Variables//
+
+var redcrystal;
+var bluecrystal;
+var greencrystal;
+var purplecrystal;
+var crystalTotal;
+var gameTotal;
+
+//Start:Initialize Variables
+
+function startGame() {
+
+// Set random values:gameTotal(from 19-120) and each crystal value (1-12)
+gameTotal = Math.floor(Math.random(120) + 19);
+
+
+//display gameTotal
+
+}
+//  PLAY-----------------------
+
+//  Which crystal selected?  
+//  Add crystal's value to crystalTotal
+//  Update crystalTotal display
+
+//  if crystalTotal < gameTotal, keep playing 
+
+// else  game over.....
+//      if crystalTotal=gameTotal  you WIN  update win count win++
+//      else you LOSE update lose count lose++
+//      display result win/lose msg
+//      update scoreboard win/loss
+//  start over:  call start function - zeros out crystals/gametotal,
+//  PLAY again
+
+
